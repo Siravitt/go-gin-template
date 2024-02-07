25a3721a30cce0291ca672881a4197bc982d3235
Siravitt <siravitt.tan@gmail.com>
2024-02-07 15:10:27 +0700

    test: complete script make setup

--- scripts/setup-pre-commit.sh
+++ scripts/setup-pre-commit.sh
@@ -2,11 +2,11 @@
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-cd ROOT_DIR
+cd $ROOT_DIR
 
 if pre-commit install --hook-type pre-commit --config .pre-commit-config.yaml
 then
-    printf "✅ DONE: config pre-commit"
+    printf "✅ DONE: config pre-commit\n"
 else
-    printf "❌ FAIL: config pre-commit"
+    printf "❌ FAIL: config pre-commit\n"
 fi
