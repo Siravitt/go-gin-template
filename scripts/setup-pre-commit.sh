b41405afef57a3c5f4105c4de9db1a28f1c81953
Siravitt <siravitt.tan@gmail.com>
2024-02-07 15:16:35 +0700

    add: add pre-push config and script

--- scripts/setup-pre-commit.sh
+++ scripts/setup-pre-commit.sh
@@ -4,9 +4,18 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 
 cd $ROOT_DIR
 
+# set: Setting pre-commit config file
 if pre-commit install --hook-type pre-commit --config .pre-commit-config.yaml
 then
     printf "✅ DONE: config pre-commit\n"
 else
     printf "❌ FAIL: config pre-commit\n"
+fi
+
+# set: Setting pre-push config file
+if pre-commit install --hook-type pre-commit --config .pre-push-config.yaml
+then
+    printf "✅ DONE: config pre-push\n"
+else
+    printf "❌ FAIL: config pre-push\n"
 fi
